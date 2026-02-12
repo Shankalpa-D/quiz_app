981fab24c3d7e5798f9da9a4ec8a755d97a917b7
Shankalpa-D <DuwadiShankalpa@gmail.com>
2026-02-12 11:34:05 -0500

    made the quiz questions and made the answers too.

---
+++ lib/data/questions.dart
@@ -0,0 +1,45 @@
+import 'package:quiz_app/models/quiz_questions.dart';
+
+const questions = [
+  QuizQuestions('who is the captain of the straw hat pirates?', [
+    'monkey d. luffy',
+    'roronoa zoro',
+    'sanji',
+    'trafalgar law',
+  ]),
+
+  QuizQuestions('what is the name of luffy\'s pirate crew?', [
+    'straw hat pirates',
+    'red hair pirates',
+    'whitebeard pirates',
+    'blackbeard pirates',
+  ]),
+
+  QuizQuestions('what power did luffy eat?', [
+    'gum gum fruit',
+    'flame flame fruit',
+    'ice ice fruit',
+    'dark dark fruit',
+  ]),
+
+  QuizQuestions('who is the swordsman of the crew?', [
+    'roronoa zoro',
+    'usopp',
+    'franky',
+    'brook',
+  ]),
+
+  QuizQuestions('what is the name of the cook on the crew?', [
+    'sanji',
+    'chopper',
+    'jinbe',
+    'ace',
+  ]),
+
+  QuizQuestions('who is luffy\'s brother?', [
+    'portgas d. ace',
+    'shanks',
+    'mihawk',
+    'buggy',
+  ]),
+];
