31562973753b97c1d9a785cc0ba42cd137403813
Shankalpa-D <DuwadiShankalpa@gmail.com>
2026-03-23 20:46:38 -0400

    made it so the questions numbers now have a circle around them.

--- lib/question_summary.dart
+++ lib/question_summary.dart
@@ -6,25 +6,41 @@ class QuestionSummary extends StatelessWidget {
   final List<Map<String, Object>> summaryData;
 
   @override
-  Widget build(context){
+  Widget build(context) {
     return Column(
-      children: summaryData.map(
-        (data){
-          return Row(
-            children:[
-              Text(((data['question_index'] as int) +1).toString()),
-              Column(
-                children:[
+      children: summaryData.map((data) {
+        final isCorrectAnswer = data['user_answer'] == data['correct_answer'];
+
+        return Row(
+          children: [
+            Container(
+              width: 30,
+              height: 30,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: isCorrectAnswer ? Colors.green : Colors.red,
+              ),
+              child: Text(
+                ((data['question_index'] as int) + 1).toString(),
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
                   Text(data['question'] as String),
-                  const SizedBox(height:5,),
+                  const SizedBox(width: 5),
                   Text(data['user_answer'] as String),
                   Text(data['correct_answer'] as String),
                 ],
-              )
-            ],
-          );
-        }
-      ).toList(),
+              ),
+            ),
+          ],
+        );
+      }).toList(),
     );
   }
 }
